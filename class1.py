e090303e8b220c67e2ef60f0382c57f7e9e404b6
Khanam <afroza.csecu@gmail.com>
2025-09-02 21:13:00 +0600

    Initial commit

---
+++ class1.py
@@ -0,0 +1,77 @@
+# print("Batch 113")
+# a = input("Enter your name:")
+# b = input("Enter your  age:")
+# print(f'My name is {a} & my age is {b}')
+
+
+# print("afroza")
+
+# a=5
+# print(a)
+
+# a=10
+# b=20
+# print(a+b)
+# _a=67
+# print(_a)
+
+# #text data type 
+# text= "Bangladesh"
+# print(text)
+# print(type(text))
+
+# # number data type
+
+# number=19
+# number2 =10.50
+# print(number)
+# print(type(number))
+# print(type(number2))
+
+# # complex number
+# number=1j
+# print(number)
+# print(type(number))
+
+# a= 'Bangladesh'
+# b=20
+# print((a)+str(b))
+
+
+# # input always strinh  hixebe input nei..
+# # tai eta ke type castiing korte hoy...
+
+# a1 = input("Enter First number:")
+# b1= input("Enter second number:")
+
+# evahbe  o  newa jai..
+a1 = int(input("Enter First number:"))
+b1=int(input("Enter second number:"))
+
+# print(int(a1)+int(b1))
+
+list1=[2,3,'bangladesh']
+print(type(list1))
+print(list1[0])
+
+list2=(2,3,'bangladesh')
+print(type(list2))
+print(list2[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ class2.py
@@ -0,0 +1,121 @@
+# print('i love "Bangldesh"')
+
+# upper and lower function
+# text1 = "i love bangladseh"
+# # print(text1.lower())
+# # print(text1.upper())
+# # print(text1.capitalize())
+# # print(text1.title())
+# print(text1.swapcase())
+
+# slicing
+# print(text1[2:6])
+# print(text1[0:])
+
+# # string negative slicing...
+# print(text1[-16:-6])
+
+# # fimding index number of a strong
+# print(text1.index('b'))
+# print(text1.index('a'))
+
+# # index number finding using find function
+
+# print(text1.find('g'))
+# print(text1.index('b'))
+
+# # finding string length
+# print(len(text1))
+# # finding occurance
+# print(text1.count('a'))
+
+# # string repalcing
+# print(text1.replace('bangladseh','amercia'))
+
+# sting spliting
+# text="amr nm jni na"
+# text1='amr-nm-jni-na'
+# b= text.split()
+# b=text1.split()
+# print(b)
+# print(b)
+
+# print(text1.split())
+# print(text1.split("-"))
+
+# # string striping/trimming
+# a="       i love bangladesh      "
+# print(a.strip())
+
+# a='------i love bangladesh------'
+# print(a.strip('-'))
+
+#f string
+# a = input("Enter your number:")
+# b= input("Enter your age:")
+# c=input("enter your designation: ")
+# print(f'my name is {a} , i am {b} years old & my designation is {c}')
+
+# # multiline string
+# a1 =''' i love 
+# bangladesh  and this is my homeland
+# '''
+# print(a1)
+
+# # escape sequence
+# print('it\'s a cat')
+# print('it\\s a cat')
+# print("its our new \n bangladesh")
+# print("its our new \tcountry")
+# print("its our   new country")
+# #  ek ghor porimar space komanor jnoo...
+
+# print("its our   \bnew country")
+
+# a = input("Enter your name:").title()
+# b= input("Enter your age:")
+# # c=input("enter your designation: ").capitalize()
+# c=input("enter your designation: ").upper()
+
+# print(f'my name is {a} , i am {b} years old & my designation is {c}')
+
+
+ 
+# a =("i love bangldesh").upper()
+# print(a)
+# x=print(a.split())
+# print(len(x))
+
+# finding total length of string wthous space
+
+a =("i love bangldesh")
+print(len(a))
+x1="".join(a.split())
+print(x1)
+print(len(x1))
+
+x2=" ".join(a.split())
+print(x2)
+print(len(x2))
+
+x3="#".join(a.split())
+print(x3)
+print(len(x3))
+
+# string concatenation
+s1 ="i love"
+s2="bangladesh"
+print(s1,s2)
+print(s1+s2)
+print(s1+ " " + s2)
+
+# jdi string er mjkhane kno kichu  add korte chai..
+s1= "i love bangladesh"
+# i want to add "the"  before bangaldesh
+s2 = s1[0:7] +"the " +s1[7:]
+print(s2)
+
+
+
+
+
